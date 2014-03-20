8e8a6e4ea1a7dbb7bb658046db73eb45e91e51cf
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2014-03-20 22:15:49 +0100

    Make normal method calls into super-calls!

--- lib/discombobulator.rb
+++ lib/discombobulator.rb
@@ -1,12 +1,24 @@
 require "discombobulator/version"
 require "discombobulator/config"
+require "discombobulator/super_call"
 
-Discombobulator::Config.instance.add_plugin(lambda { puts "You made it this time" })
+$DISCOMBOBULATOR_SAFETY_FEATURE = 42
+
+Discombobulator::Config.instance.add_plugin(
+  ->(meth, *args, &block) do
+    puts "You made it this time!"
+    Discombobulator.new
+  end
+)
 
 class Discombobulator
-  def method_missing(_)
-    plugins.sample.call
-    Discombobulator.new
+  def self.method_missing(meth, *args, &block)
+    self.new.method_missing(meth, *args, &block)
+  end
+
+  def method_missing(meth, *args, &block)
+    plugin = plugins.sample
+    plugin.call(meth, *args, &block)
   end
 
   private
